a408d8c656e7e7a431ea950944c96467f0f755d6
padonak007 <itchernikow@gmail.com>
2017-05-12 16:58:27 +0100

    2

--- script.js
+++ script.js
@@ -4,24 +4,32 @@ $("<div/>")
     .appendTo($("body"));
 
 //Adding random shit
-var amountOfShit =3;
-for (var i=0; i<=amountOfShit; i++){
+var amountOfShit = 3;
+var arrows = {
+    left: 37,
+    up: 38,
+    right: 39,
+    down: 40,
+}
+var step = 10;
+
+for (var i = 0; i <= amountOfShit; i++) {
     $("<div/>")
-    .addClass("divClass")
-    .css("top", randomCoord())
-    .css("left", randomCoord())
-    .text("Someshit"+i)
-    .attr("data-active","true")
-    .appendTo($(".edgeBorder"));
+        .addClass("divClass")
+        .css("top", randomCoord())
+        .css("left", randomCoord())
+        .text("Someshit" + i)
+        .attr("data-active", "false")
+        .appendTo($(".edgeBorder"));
 }
 //Adding arrow buttons
-var buttons = ["&rarr;","&larr;","&uarr;","&darr;" ];
-for (var i=0;i<buttons.length;i++){
+var buttons = ["&rarr;", "&larr;", "&uarr;", "&darr;"];
+for (var i = 0; i < buttons.length; i++) {
     $("<button/>")
-    .attr("id", "buttonRight")
-    .addClass("buttonClass")
-    .html(buttons[i])
-    .appendTo($(".edgeBorder"));
+        .attr("id", "buttonRight")
+        .addClass("buttonClass")
+        .html(buttons[i])
+        .appendTo($(".edgeBorder"));
 }
 
 //Adding "Active button"
@@ -36,40 +44,33 @@ $(".flyingShit").click(function (arg) {
     $(event.target.parentNode).attr("data-active", "true");
 });
 
-var arrows = {
-    left: 37,
-    up:38,
-    right: 39,
-    down: 40,
-}
-
-
 $("body").keydown(function (arg) {
-    switch (arg.keyCode){
+    var $active = $("[data-active = true]");
+    switch (arg.keyCode) {
         //MoveRight
         case arrows.right:
-        var currPos = $("[data-active=true]").position().left;
-        var newPos = (currPos + 10).toString() + "px";
-        $("[data-active = true]").css({ "left": newPos });
-        break;
+            var currPos = $active.position().left;
+            var newPos = (currPos + step);
+            $active.css({ "left": newPos });
+            break;
         //MoveLeft
         case arrows.left:
-        var currPos = $("[data-active=true]").position().left;
-        var newPos = (currPos - 10).toString() + "px";
-        $("[data-active=true]").css({ "left": newPos });
-        break;
+            var currPos = $active.position().left;
+            var newPos = (currPos - step);
+            $active.css({ "left": newPos });
+            break;
         //MoveUp
         case arrows.up:
-        var currPos = $("[data-active=true]").position().top;
-        var newPos = (currPos - 10).toString() + "px";
-        $("[data-active=true]").css({ "top": newPos });
-        break;
+            var currPos = $active.position().top;
+            var newPos = (currPos - step);
+            $active.css({ "top": newPos });
+            break;
         //MoveDown
         case arrows.down:
-        var currPos = $("[data-active=true]").position().top;
-        var newPos = (currPos + 10).toString() + "px";
-        $("[data-active=true]").css({ "top": newPos });
-        break;
+            var currPos = $active.position().top;
+            var newPos = (currPos + step);
+            $active.css({ "top": newPos });
+            break;
     }
 });
 
