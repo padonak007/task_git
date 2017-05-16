08eff7ffb85ced904e82454f5f43c1c39f43ec69
padonak007 <itchernikow@gmail.com>
2017-05-16 13:13:05 +0100

    tasks 9-14

--- script.js
+++ script.js
@@ -3,6 +3,14 @@ $("<div/>")
     .addClass("edgeBorder")
     .appendTo($("body"));
 
+//Add div button
+$("<button/>")
+    .text("Add Div")
+    .appendTo($(".edgeBorder"))
+    .click(function (event) {
+        createDivWithButtons();
+    });
+
 //Adding random shit
 var amountOfShit = 3;
 var arrows = {
@@ -17,123 +25,133 @@ var step = 10;
 var buttons = ["&rarr;", "&larr;", "&uarr;", "&darr;"];
 
 for (var i = 0; i <= amountOfShit; i++) {
-    $("<div/>")
+    createDivWithButtons();
+}
+
+$(".buttonClass").appendTo($(".edgeBorder"));
+//Adding activation button
+
+function createDivWithButtons() {
+    var createdDiv = $("<div/>")
         .addClass("divClass")
         .css("top", randomCoord())
         .css("left", randomCoord())
         .attr("data-active", "false")
-        .appendTo($(".edgeBorder"));
+        .appendTo($(".edgeBorder"))
     $("<span/>")
-        .text("Someshit" + i)
-        .appendTo($("[data-active = false]").get(i));
-}
-//Adding arrow buttons
-
-for (var i = 0; i < buttons.length; i++) {
+        .text("Someshit")
+        .appendTo($(createdDiv));
     $("<button/>")
-        .attr("data-buttonDirection", buttons[i])
-        .addClass("buttonClass")
-        .html(buttons[i])
-        .appendTo($(".edgeBorder"));
+        .text("Activate")
+        .addClass("flyingShit")
+        .appendTo($(createdDiv))
+        .click(function (event) {
+            $(".divClass").removeAttr("data-active");
+            $(event.target).parent().attr("data-active", "true");
+            $(event.target).parent().css({ "box-shadow": "0 0 10px red" })
+            $(".buttonClass").removeAttr("disabled");
+        });
+    $("<button/>")
+        .text("Deactivate")
+        .addClass("flyingShit")
+        .appendTo($(createdDiv))
+        .click(function (event) {
+            $(".divClass").removeAttr("data-active");
+            $(event.target).parent().css({ "box-shadow": "" });
+            $(".buttonClass").attr("disabled","");
+        });
 }
-
-//Adding "Active button"
-$("<button/>")
-    .text("Activate")
-    .addClass("flyingShit")
-    .appendTo($(".divClass"));
-//Shit activation
-$(".flyingShit").click(function (arg) {
-    alert("Button:" + $(arg.target).parent().children("span").text() + " is activated");
-    $(".divClass").removeAttr("data-active");
-    $(arg.target.parentNode).attr("data-active", "true");
-});
 
 // Move items on button press
 $("body").keydown(function (arg) {
-    //var $active = $("[data-active = true]");
-    //var pos = $active.position();
-    switch (arg.keyCode) {
-        //MoveRight
-        case arrows.right:
-            moveRight();
-            break;
-        //MoveLeft
-        case arrows.left:
-            moveLeft();
-            break;
-        //MoveUp
-        case arrows.up:
-            moveUp();
-            break;
-        //MoveDown
-        case arrows.down:
-            moveDown();
-            break;
+    if (checkActiveElement()) {
+        switch (arg.keyCode) {
+            //MoveRight
+            case arrows.right:
+                moveRight();
+                break;
+            //MoveLeft
+            case arrows.left:
+                moveLeft();
+                break;
+            //MoveUp
+            case arrows.up:
+                moveUp();
+                break;
+            //MoveDown
+            case arrows.down:
+                moveDown();
+                break;
+        }
     }
 });
 // Move items on clicks
-$(".buttonClass").click(function (arg){
-    switch ($("[data-buttonDirection]").attr("data-buttonDirection")){
-        //MoveRight
-        case "&rarr;":
-            moveRight();
-            break;
-        //MoveLeft
-        case "&larr;":
-            moveLeft();
-            break;
-        //MoveUp
-        case "&uarr;":
-            moveUp();
-            break;
-        //MoveDown
-        case "&darr;":
-            moveDown();
-            break;
+$(".buttonClass").click(function (arg) {
+    if (checkActiveElement()) {
+        switch ($(arg.target).attr("data-buttonDirection")) {
+            //MoveRight
+            case "&rarr;":
+                moveRight();
+                break;
+            //MoveLeft
+            case "&larr;":
+                moveLeft();
+                break;
+            //MoveUp
+            case "&uarr;":
+                moveUp();
+                break;
+            //MoveDown
+            case "&darr;":
+                moveDown();
+                break;
+        }
     }
 });
 
 function moveRight(arg) {
-    if ($("[data-active = true]").length == 0){
-        alert ("Click 'Activate' on any button")
-    } else {
     var currPos = $("[data-active = true]").position().left;
     var newPos = currPos + step;
     $("[data-active = true]").css({ "left": newPos });
-    }
 }
 
 function moveLeft(arg) {
-    if ($("[data-active = true]").length == 0){
-        alert ("Click 'Activate' on any button")
-    } else {
     var currPos = $("[data-active = true]").position().left;
     var newPos = currPos - step;
     $("[data-active = true]").css({ "left": newPos });
-    }
 }
 
 function moveUp(arg) {
-    if ($("[data-active = true]").length == 0){
-        alert ("Click 'Activate' on any button")
-    } else {
     var currPos = $("[data-active = true]").position().top;
     var newPos = currPos - step;
     $("[data-active = true]").css({ "top": newPos });
-    }
+
 }
 
 function moveDown(arg) {
-    if ($("[data-active = true]").length == 0){
-        alert ("Click 'Activate' on any button")
-    } else {
     var currPos = $("[data-active = true]").position().top;
     var newPos = currPos + step;
     $("[data-active = true]").css({ "top": newPos });
-    }
 }
 
 function randomCoord() {
     return (Math.random() / 2).toString().substring(2, 5) + "px";
+};
+
+//Check for active element
+
+function checkActiveElement() {
+    var elementExists = $("[data-active = true]").length != 0;
+    if (elementExists) {
+        return true;
+    }
+    //alert("Click 'Activate' on any button");
+    return false;
+
+}
+
+function elementActivation(event) {
+    alert("Button:" + $(event.target).parent().children("span").text() + " is activated");
+    $(".divClass").removeAttr("data-active");
+    $(event.target.parentNode).attr("data-active", "true");
 };
